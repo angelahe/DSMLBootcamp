d080802c419def0403da3086146af218e234471b
angela <angela@aviak.ca>
2021-02-09 19:11:42 -0700

    getting values from numpy arrays

---
+++ S05_numpy_index.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+#Creating sample array
+arr = np.arange(0,11)
+
+print(f'array from 0 to 10: {arr}')
+
+# getting elements of array is similar to python lists
+print(f'element at index 8 is {arr[8]}')
+print(f'elements from index 1 to 4: {arr[1:5]}')
