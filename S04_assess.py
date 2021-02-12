d2913df0b2b86acb91ab057026001b31a4799a93
angela <angela@aviak.ca>
2021-02-12 11:20:00 -0700

    practice accessing elements in lists

--- S04_assess.py
+++ S04_assess.py
@@ -1,7 +1,23 @@
+# what is 7 to the power of 2
 print(f'7**2 is : {7**2}')
 
+# split this string into a list
 s = 'Hi there Sam!'
 
 splitstring = s.split()
-print(splitstring)
+print(f'string split into list: {splitstring}')
+
+planet = "Earth"
+diameter = 12742
+
+print(f'the diameter of {planet} is {diameter}')
+print('the diameter of {} is {}'.format(planet, diameter))
+
+# get hello from these nested lists:
+
+lst = [1,2,[3,4],[5,[100,200,['hello']],23,11],1,7]
+print(f'getting hello from the list: {lst[3][1][2][0]}')
+
+d = {'k1':[1,2,3,{'tricky':['oh','man','inception',{'target':[1,2,3,'hello']}]}]}
+print(f"get hello again: {d['k1'][3]['tricky'][3]['target'][3]}")
 
