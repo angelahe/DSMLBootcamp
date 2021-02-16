b0d68e930171c1eb4df96f110241a317591b32cd
angela <angela@aviak.ca>
2021-02-15 21:20:17 -0700

    getting elements from 2d array

--- S05_numpy_index.py
+++ S05_numpy_index.py
@@ -8,7 +8,7 @@ print(f'array from 0 to 10: {arr}')
 # getting elements of array is similar to python lists
 print(f'element at index 8 is {arr[8]}')
 print(f'elements from index 1 to 4: {arr[1:5]}')
-
+print(f'first 5 in the array: {arr[:5]}')
 # numpy arrays can broadcast
 # ie set a value with an index range
 arr[0:5]=100
@@ -28,3 +28,13 @@ print(f'original arr was changed too:\n {arr}')
 # to get a copy, you have to be explicit!
 arr_copy = arr.copy()
 print(f'explicit copy of array: {arr_copy}')
+
+# index a 2d array
+arr_2d = np.array(([5,10,15],[20,25,30],[35,40,45]))
+print(f'arr_2d is: {arr_2d}')
+
+# index row
+print(f'index 1 row: {arr_2d[1]}')
+
+# Getting individual element value
+print(f'get 2nd row 1st element: {arr_2d[1][0]}')
