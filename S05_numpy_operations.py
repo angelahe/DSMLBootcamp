c119416401ed4df127bb10b733eaba19dd72772a
angela <angela@aviak.ca>
2021-02-22 10:15:00 -0700

    perform operations on all elements of array

---
+++ S05_numpy_operations.py
@@ -0,0 +1,8 @@
+import numpy as np
+arr = np.arange(0,10)
+
+# perform operations on arrays
+print(f'arr is: {arr}\narr + arr is:\n{arr + arr}')
+print(f'arr is: {arr}\narr - arr is:\n{arr - arr}')
+print(f'arr is: {arr}\narr * arr is:\n{arr * arr}')
+print(f'arr is: {arr}\narr + 100 is:\n{arr + 100}')
