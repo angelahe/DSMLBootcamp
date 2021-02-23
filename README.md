44de47a9a9c24f20bd781d5dd24435d8ed8f7b07
angela <angela@aviak.ca>
2021-02-23 11:15:00 -0700

    universal array functions in numpy

--- README.md
+++ README.md
@@ -8,6 +8,8 @@ conda info --envs
 conda install numpy
 
 ## further reading
+### managing conda environments
 https://conda.io/projects/conda/en/latest/user-guide/tasks/manage-environments.html
-
+### universal functions
+https://numpy.org/doc/stable/reference/ufuncs.html
 
--- S05_numpy_operations.py
+++ S05_numpy_operations.py
@@ -6,3 +6,13 @@ print(f'arr is: {arr}\narr + arr is:\n{arr + arr}')
 print(f'arr is: {arr}\narr - arr is:\n{arr - arr}')
 print(f'arr is: {arr}\narr * arr is:\n{arr * arr}')
 print(f'arr is: {arr}\narr + 100 is:\n{arr + 100}')
+print(f'arr is: {arr}\narr ** arr is:\n{arr ** arr}')
+print(f'numpy div by 0 gives warning, not error, outputs nan: {arr/arr}')
+
+# universal array functions
+
+print(f'square root of array: {np.sqrt(arr)}')
+print(f'exponential (e^) of array: {np.exp(arr)}')
+print(f'max of array is (same as arr.max()): {np.max(arr)}')
+print(f'sine of array: {np.sin(arr)}')
+print(f'logarithm of array: {np.log(arr)}')
