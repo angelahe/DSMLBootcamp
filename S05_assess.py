4aed4694bd257307cb0db1445a64a54d9863567f
angela <angela@aviak.ca>
2021-02-25 10:21:36 -0700

    practice indexing and slicing arrays

--- S05_assess.py
+++ S05_assess.py
@@ -30,3 +30,16 @@ print(f'random 25 std normal distn:\n {np.random.randn(25)}')
 print(f'0.01 to 1.0 2d array: {np.arange(1,101).reshape(10,10) /100}')
 # array of 20 linearly spaced points between 0 and 1
 print(f'20 linearly spaced points:\n {np.linspace(0,1,20)}')
+
+# indexing and selection
+matrix = np.arange(1,26).reshape(5,5)
+print(f'matrix is:\n{matrix}')
+print(f'slice of array: {matrix[2:,1:]}')
+print(f'element of array 20: {matrix[3,4]}')
+print(f'more array slicing: {matrix[:3,1:2]}')
+print(f'get row 5: {matrix[4,:]}')
+print(f'get rows 3 and 4: {matrix[3:5,:]}')
+
+print(f'sum of all values in matrix: {matrix.sum()}')
+print(f'std deviation of matrix: {matrix.std()}')
+print(f'sum of columns in matrix: {matrix.sum(axis=0)}')
