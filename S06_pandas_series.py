06ad2403c79041a674c7dbb2aa5bd4f1f581625e
angela <angela@aviak.ca>
2021-02-27 18:22:36 -0700

    pandas data series using lists and dictionaries

--- S06_pandas_series.py
+++ S06_pandas_series.py
@@ -9,4 +9,10 @@ arr = np.array([10,20,30])
 d = {'a':10,'b':20,'c':30}
 
 print(f'the series:\n{pd.Series(data=my_list)}')
-print(f'the series:\n{pd.Series(data=my_list, index=labels)}')
+print(f'the series using labels:\n{pd.Series(data=my_list, index=labels)}')
+print(f'series simplified using labels:\n{pd.Series(my_list, labels)}')
+
+print(f'series w numpy array:\n{pd.Series(arr)}')
+print(f'series w numpy array and labels:\n{pd.Series(arr, labels)}')
+
+print(f'series using dictionary:\n{pd.Series(d)}')
