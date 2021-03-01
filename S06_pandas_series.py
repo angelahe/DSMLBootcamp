12213350b28b8e8bd27a25be5db5d28cc5ad566b
angela <angela@aviak.ca>
2021-02-28 18:49:13 -0700

    can create series on other datatypes including functions

--- S06_pandas_series.py
+++ S06_pandas_series.py
@@ -16,3 +16,9 @@ print(f'series w numpy array:\n{pd.Series(arr)}')
 print(f'series w numpy array and labels:\n{pd.Series(arr, labels)}')
 
 print(f'series using dictionary:\n{pd.Series(d)}')
+
+# note you can put any data type into a pandas series
+
+print(f'series on string datatype:\n{pd.Series(labels)}')
+print(f'series on functions:\n{pd.Series([sum, print, len])}')
+
