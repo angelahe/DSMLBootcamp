db1658c24d5ebaa58b476f0d0549ccafe0a11dc1
angela <angela@aviak.ca>
2021-03-09 09:51:08 -0700

    using groupby on dataframe

--- S06_groupby.py
+++ S06_groupby.py
@@ -10,6 +10,25 @@ print(f"dataframe is:\n{df}")
 
 by_company = df.groupby('Company')
 
+# get sum, average, std deviations as dataframe
 print(f"total sales by company:\n{by_company.sum()}")
 
-print(f"mean sales by company:\n{by_company.mean()}")
+print(f"average sales by company:\n{by_company.mean()}")
+
+print(f"standard deviations by company:\n{by_company.std()}")
+
+print(f"total sales for FB: {by_company.sum().loc['FB']}")
+
+print(f"total sales for FB in one line: {df.groupby('Company').sum().loc['FB']}")
+
+print(f"count of people and sales per company: {df.groupby('Company').count()}")
+
+print(f"max salesperson (alpha) per company: {df.groupby('Company').max()}")
+
+print(f"min salesperson (alpha)per company: {df.groupby('Company').min()}")
+
+print(f"describe for info on dataset: {df.groupby('Company').describe()}")
+
+print(f"describe for info on dataset: {df.groupby('Company').describe().transpose()}")
+
+print(f"transposed describe info on GOOG: {by_company.describe().transpose()['GOOG']}")
