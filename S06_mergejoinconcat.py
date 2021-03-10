1a91871aac2ea6848ed7fdf0718392db97f19e73
angela <angela@aviak.ca>
2021-03-10 06:41:10 -0700

    merge with inner join, outer join, right join, left join

--- S06_mergejoinconcat.py
+++ S06_mergejoinconcat.py
@@ -37,3 +37,21 @@ right = pd.DataFrame({'key': ['K0', 'K1', 'K2', 'K3'],
 print(f"left dataframe\n{left}to merge with right\n{right}")
 print(f"merge dataframes using logic:\n{pd.merge(left,right,how='inner',on='key')}")
 
+left = pd.DataFrame({'key1': ['K0', 'K0', 'K1', 'K2'],
+                     'key2': ['K0', 'K1', 'K0', 'K1'],
+                     'A': ['A0', 'A1', 'A2', 'A3'],
+                     'B': ['B0', 'B1', 'B2', 'B3']})
+
+right = pd.DataFrame({'key1': ['K0', 'K1', 'K1', 'K2'],
+                      'key2': ['K0', 'K0', 'K0', 'K0'],
+                      'C': ['C0', 'C1', 'C2', 'C3'],
+                      'D': ['D0', 'D1', 'D2', 'D3']})
+
+print(f"left dataframe\n{left}to merge with right\n{right}")
+print(f"left merge with right on key1 and key2:\n{pd.merge(left, right, on=['key1', 'key2'])}")
+
+print(f"merge left and right by outer join on key1 and key2:\n{pd.merge(left, right, how='outer', on=['key1', 'key2'])}")
+
+print(f"merge left and right by right join on key1 and key2:\n{pd.merge(left, right, how='right', on=['key1', 'key2'])} ")
+
+print(f"merge left and right by left join on key1 and key2:\n{pd.merge(left, right, how='left', on=['key1', 'key2'])}")
