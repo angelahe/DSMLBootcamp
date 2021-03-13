9e3b2efeb563ff7e0d1951b3012459cc94a5e99f
angela <angela@aviak.ca>
2021-03-13 09:25:57 -0700

    selecting data review

--- README.md
+++ README.md
@@ -15,6 +15,10 @@ https://conda.io/projects/conda/en/latest/user-guide/tasks/manage-environments.h
 https://numpy.org/doc/stable/reference/ufuncs.html
 
 # notes
+for viewing the jupyter notebooks associated with the course:
+> conda activate nlp_course
+> jupyter notebook
+
 pandas is open source library built on top of NumPy
 fast analysis and data cleaning and preparation
 visualization features
--- S06_operations.py
+++ S06_operations.py
@@ -9,3 +9,11 @@ print(f"unique values in column2: {df['col2'].unique()}")
 print(f"# of unique values in col2:{len(df['col2'].unique())}")
 print(f"# of unique values in col2 method2:{df['col2'].nunique()}")
 print(f"unique values and how many times they show up:\n{df['col2'].value_counts()}")
+
+# selecting data
+#Select from DataFrame using criteria from multiple columns
+
+print(f"return data where val in col1 > 2:\n{df[df['col1']>2]}")
+print(f"remember this is just returning a series of true and false:\n{df['col1']>2}")
+newdf = df[(df['col1']>2) & (df['col2']==444)]
+print(f"where col1 > 2 and col2 is 444\n{newdf}")
